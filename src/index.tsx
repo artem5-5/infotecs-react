e5fcd8c07ed0129e5793adefe035d99411f788b8
artem-badasyan <133artemka123@gmail.com>
2026-02-22 19:47:44 +0300

    [TASK-01] Setup project infrastructure. Configured Webpack + TypeScript environment. Installed core dependencies. Added entry point and dev server configuration.

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import App from './App'
+
+const queryClient = new QueryClient()
+
+ReactDOM.render(
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>,
+  document.getElementById('root')
+)
